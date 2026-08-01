e9e78b4ccdeba3b9e7be840ee363e363028141b1
Suyanshi Patel <24cs077@charusat.edu.in>
2026-08-01 17:12:59 +0530

    Add student portfolio with Header, About, Skills, Footer, and Navbar components

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Footer from "./components/Footer";
+
+
+function App() {
+  const skills = ["HTML", "CSS", "JavaScript", "React", "Node.js", "Git"];
+
+  return (
+    <div className="app">
+      <Header name="Suyanshi Patel" themeColor="#0f766e" />
+      <Navbar />
+      <main className="main-content">
+        <About course="Computer Science Engineering" />
+        <Skills skillList={skills} />
+      </main>
+      <Footer email="suyanshi@gmail.com" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#skills">Skills</a>
+      <a href="#contact">Contact</a>
+    </nav>
+  );
+}
+
+export default Navbar;
